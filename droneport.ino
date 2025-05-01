2af5c7a5798c7d93506b28b9e4739e36b9f0ed89
Glebas4 <gleb.gurov.jr@gmail.com>
2025-05-02 00:12:34 +0300

    Create droneport.ino

---
+++ droneport.ino
@@ -0,0 +1,168 @@
+#include <WiFi.h>
+#include <FastLED.h>
+#include "GyverStepper.h"
+
+#define NUM_LEDS 256 
+#define PIN 4 // LED
+#define buz 1 //BUZZER
+
+const char* ssid = "ASUS_20";
+const char* password =  "harmony_9957";
+
+WiFiServer wifiServer(80);
+CRGB leds[NUM_LEDS];
+GStepper< STEPPER4WIRE> stepperL(2038, 5, 7, 6, 8);  //Если смотреть на дронпорт со стороны матрицы - Левый
+GStepper< STEPPER4WIRE> stepperR(2038, 10, 18, 9, 19);  //Если смотреть на дронпорт со стороны матрицы - Правый
+
+
+void setup() 
+{
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {delay(500);}
+
+  stepperL.autoPower(true);
+  stepperR.autoPower(true);
+  stepperL.setMaxSpeed(400);
+  stepperR.setMaxSpeed(400);
+  stepperL.reverse(true);
+
+  FastLED.addLeds <WS2812, PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(40);
+}
+
+
+// КАРТИНКИ
+void verx()
+{
+  FastLED.clear(); 
+  for(int x=71; x<184 ) {x=x+16; leds[x] = CRGB::Green}
+  for(int x=72; x<185 ) {x=x+16; leds[x] = CRGB::Green}
+  leds[169] = CRGB::Green;//verx
+  leds[166] = CRGB::Green;
+  leds[149] = CRGB::Green;
+  leds[154] = CRGB::Green;
+  FastLED.show();
+
+}
+
+
+void vniz()
+{
+  FastLED.clear(); 
+  for(int x=71; x<184 ) {x=x+16; leds[x] = CRGB::Red}
+  for(int x=72; x<185 ) {x=x+16; leds[x] = CRGB::Red}
+  leds[86] = CRGB::Red;//vniz
+  leds[89] = CRGB::Red;
+  leds[106] = CRGB::Red;
+  leds[101] = CRGB::Red;
+  FastLED.show();
+}
+
+
+void batka()
+{
+  FastLED.clear(); 
+  for (int x =149; x<158; x++) {leds[x] = CRGB::Red;}  
+  for (int x =98; x<107; x++) {leds[x] = CRGB::Red;}  
+  leds[125] = CRGB::Red;
+  leds[130] = CRGB::Red;
+  leds[115] = CRGB::Red;
+  leds[116] = CRGB::Red;
+  leds[117] = CRGB::Red;
+  leds[138] = CRGB::Red;
+  leds[139] = CRGB::Red;
+  leds[140] = CRGB::Red;
+  FastLED.show();
+}
+
+
+void open()
+{
+  stepperR.setTargetDeg(360); 
+  stepperL.setTargetDeg(360); 
+}
+
+
+void close()
+{
+  stepperR.setTargetDeg(0); 
+  stepperL.setTargetDeg(0);
+}
+
+
+void zvuk() 
+{
+  tone(buz, 1900);
+  delay(500);
+  noTone(buz);
+  delay(500);
+  tone(buz, 1900, 1000);
+  delay(500);
+  noTone(buz);
+}
+
+
+void ledoff()
+{
+  FastLED.clear(); 
+  FastLED.show();
+}
+
+
+void loop() 
+{  
+  stepperL.tick();
+  stepperR.tick();
+
+  WiFiClient client = wifiServer.available();
+
+  if (client.connected())
+     {
+     if (client.available()>0) 
+        {
+          char msg = client.read();
+          int data = msg -'0' ;
+
+          switch(data)
+                {
+                case 1: 
+                      open(); 
+                      while (stepperL.tick(); && stepperR.tick();) {}    //open 
+                      client.write(1); break;                     
+                
+                case 2: 
+                      close();
+                      while (stepperL.tick(); && stepperR.tick();) {}    //close
+                      client.write(1); break;                    
+                
+                case 3: 
+                      verx(); 
+                      zvuk(); 
+                      open(); 
+                      while (stepperL.tick(); && stepperR.tick();) {}   //takeoff
+                      client.write(1); break;      
+               
+                case 4:
+                      vniz(); 
+                      zvuk(); 
+                      close();    
+                      while (stepperL.tick(); && stepperR.tick();) {}   //land
+                      client.write(1); break;
+                
+                case 5:
+                      batka(); 
+                      client.write(1); break;                           //batka
+                
+                case 6:
+                      ledoff();
+                      close();
+                      while (stepperL.tick(); && stepperR.tick();) {}   //off
+                      client.write(1); break;          
+               
+                default: break;
+                }
+         
+        }
+     }     
+}
