eeb97c52b6060476471701e78ec2686bf0be75f7
Glebas4 <gleb.gurov.jr@gmail.com>
2025-08-09 15:30:36 +0300

    Update telempub.py

--- clover/telempub.py
+++ clover/telempub.py
@@ -1,13 +1,17 @@
 import rospy
 from clover import srv
 from std_msgs.msg import Float32MultiArray
+
+
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 pub = rospy.Publisher('TELEMETRY', Float32MultiArray, queue_size=1)
+
 
 if __name__ == '__main__':
     rospy.init_node("telem_topic")
     rate = rospy.Rate(1)
 
+    
     while not rospy.is_shutdown():
         t = get_telemetry(frame_id='aruco_map')
         msgs = [t.x, t.y, t.z, t.voltage, float(t.armed)]
